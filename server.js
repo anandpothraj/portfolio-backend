e72690a9291982be9daaaad82aa7c2e4e5ce8c9e
anandpothraj <anandpothraj11052001@gmail.com>
2023-02-24 23:34:16 +0530

    Fixed cors issue

--- server.js
+++ server.js
@@ -1,32 +1,36 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const config = require("config");
 const cors = require("cors");
 const app = express();
 
+var allowedOrigins = [
+  config.get("frontend_url.production"),
+  "frontend_url.development","frontend_url.local"
+];
+
 app.use(
   cors({
-    origin: [config.get("frontend_url.production"), "http://localhost:3000"],
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
   })
 );
 
 app.use(express.json());
 
-const db = config.get("mongoURI");
-
-mongoose.set("strictQuery", false);
-mongoose.connect(db, {useNewUrlParser: true})
-  .then(() => {
-    console.log(`MongoDB connected...`);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-
 const port = process.env.PORT || 6678;
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hello servers is running").end();
+  res.status(200).send("Hello server is running").end();
 });
 
 app.use("/api/contact", require("./api/contact"));
