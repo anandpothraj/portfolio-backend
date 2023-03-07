f2e323a1e8a4f33e7a2825143c78e8ede7b5fadf
anandpothraj <anandpothraj11052001@gmail.com>
2023-03-07 16:52:51 +0530

    Added send report and send feedback route

--- api/contact.js
+++ api/contact.js
@@ -12,7 +12,7 @@ var transporter = nodemailer.createTransport({
 });
 
 router.post("/sendMessage", (req, res) => {
-  try {
+  try { 
     let reqBody = req.body;
     let requiredFields = [ "name", "email", "message" ];
     let invalidFields = [];
@@ -47,7 +47,105 @@ router.post("/sendMessage", (req, res) => {
       }
       else {
         res.status(201).json({
-          message: "message sended successfully",
+          message: "message sent successfully",
+        });
+      }
+    });
+  }
+  catch (e) {
+    console.log(`Error while send the message: ${e}`);
+    return res.status(500).json({
+      message:"There was a problem while sending the message. Please try again later.",
+    });
+  }
+});
+
+router.post("/sendReport", (req, res) => {
+  try {
+    let reqBody = req.body;
+    let requiredFields = [ "name", "email", "message" ];
+    let invalidFields = [];
+
+    requiredFields.forEach((field) => {
+      if (!isFieldPresentInRequest(reqBody, field)) {
+        invalidFields.push(field);
+      }
+    });
+
+    if (invalidFields.length > 0) {
+      return res.status(400).json({
+        message: `Error - Missing fields: ${invalidFields.join(", ")}`,
+      });
+    }
+
+    const { name, email, message } = reqBody;
+
+    var mailOptions = {
+      from:'anandpothraj11052001@gmail.com',
+      to:'pothrajanand@gmail.com',
+      subject:`${name} has reported an issue.`,
+      text:`${message}. You can contact me here ${email}`
+    }
+  
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log("There was a problem while sending message: ", error);
+        return res.status(500).json({
+          message:"There was a problem while sending message. Please try again later.",
+        });
+      }
+      else {
+        res.status(201).json({
+          message: "Issue reported successfully",
+        });
+      }
+    });
+  }
+  catch (e) {
+    console.log(`Error while send the message: ${e}`);
+    return res.status(500).json({
+      message:"There was a problem while sending the message. Please try again later.",
+    });
+  }
+});
+
+router.post("/sendFeedback", (req, res) => {
+  try {
+    let reqBody = req.body;
+    let requiredFields = [ "name", "email", "message" ];
+    let invalidFields = [];
+
+    requiredFields.forEach((field) => {
+      if (!isFieldPresentInRequest(reqBody, field)) {
+        invalidFields.push(field);
+      }
+    });
+
+    if (invalidFields.length > 0) {
+      return res.status(400).json({
+        message: `Error - Missing fields: ${invalidFields.join(", ")}`,
+      });
+    }
+
+    const { name, email, message } = reqBody;
+
+    var mailOptions = {
+      from:'anandpothraj11052001@gmail.com',
+      to:'pothrajanand@gmail.com',
+      subject:`${name} has sent you an feedback.`,
+      text:`${message}. You can contact me here ${email}`
+    }
+  
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log("There was a problem while sending message: ", error);
+        return res.status(500).json({
+          message:"There was a problem while sending message. Please try again later.",
+        });
+      }
+      else {
+        res.status(201).json({
+          message: "Feedback submitted successfully",
         });
       }
     });
